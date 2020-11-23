93554d56b5bbb983e4a110694861496a60695c26
eugene-zhary <eugenezhary05@gmail.com>
2020-11-23 22:11:31 +0200

    fix

--- Connector.cs
+++ Connector.cs
@@ -9,13 +9,39 @@ namespace MySQLWorker
 {
     public class Connector
     {
+        public string ConnectionPath { get; set; }
         public ServerInfo ServerInfo { get; set; }
 
         public Connector()
         {
             this.ServerInfo = new ServerInfo();
+            this.ServerInfo.ConnectionChanged += ConnectionChanged;
+        }
+        public Connector(ServerInfo server_info)
+        {
+            this.ServerInfo = server_info;
+            this.ServerInfo.ConnectionChanged += ConnectionChanged;
+        }
+        private void ConnectionChanged(object sender, EventArgs e)
+        {
+            if (sender is ServerInfo info) {
+                UpdateConnectionPath(info);
+            }
+        }
+        private void UpdateConnectionPath(in ServerInfo info)
+        {
+            this.ConnectionPath = $@"Data Source={info.ServerIp};Initial Catalog={info.Database};User Id = {info.Login};Password = {info.Password};Integrated Security={info.IntegratedSecurity};Connection Timeout=1;";
         }
 
-        public object[,] ReadTable() => Execute.QuerySelect("*", this.ServerInfo.TableName, this.ServerInfo.ConnectionPath);
+
+        public object[,] ReadTable()
+        {
+            try {
+                return Execute.QuerySelect("*", this.ServerInfo.TableName, this.ConnectionPath);
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+        }
     }
 }
